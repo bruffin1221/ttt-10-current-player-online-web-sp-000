70e5a27fe012d16057390c9f088753396aacf299
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-16 05:35:00 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+board=0
+  if board=2
+  board+=1
+end
+end
+
+def current_player(board)
+if  turn_count(board).even?
+  puts "O"
+else turn_count(board).odd?
+  puts "X"
+end
+end

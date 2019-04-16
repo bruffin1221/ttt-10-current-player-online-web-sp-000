2d6305dfe2443778d859fd8c8fb47e1ba9125db2
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-16 06:39:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,9 +8,7 @@ end
 def current_player(board)
 if turn_count(board)%2==0
   return "X"
-elsif turn_count(board).even? 
-  return "X"
-else turn_count(board).odd?
+else turn_count(board)%==1
   return "O"
 end
 end

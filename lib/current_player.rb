048e477451617a2bb0b13aecbd4c91f18911fc83
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-16 09:21:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,12 @@
 def turn_count(board)
-board=0
-  if board=2
-  board+=1
-end
-end
+  counter = 0
+     board.each do |spaces|
+        if spaces == "X" || spaces == "O"
+           counter += 1
+        end
+     end
+     counter
+  end
 
 def current_player(board)
  turn_count(board)%2==0 ? "O" : "X"
